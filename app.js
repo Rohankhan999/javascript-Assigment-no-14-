d643464959f778d3846e7a1284191d2ecc533f96
Rohankhan999 <rohankhan08453@gmail.com>
2024-11-16 19:34:26 +0500

    .

---
+++ app.js
@@ -0,0 +1,41 @@
+//  Chapter no 35 to 38 Function //
+
+// Question no 01 //
+
+// function showdate() {
+//     var date = new Date();
+//     return date
+// }
+// console.log(showdate());
+
+
+// Question no 02 //
+
+// function greetuser(Firstname , Lastname) {
+//     var Fullname = `${Firstname} ${Lastname}`
+//     return `Hello ${Fullname}! Welcome!!!!!!`
+// }
+// document.write("<h1>" + greetuser("Rohan","Khan") + "</h1>");
+                         
+                            //  OR
+ 
+// function greetuser(){
+//     var Firstname = prompt('Enter your First Name');
+//     var Lastname = prompt('Enter your Last Name');
+//     const Fullname = `${Firstname} ${Lastname}`;
+//     alert(`Hello ${Fullname}! Welcome!!!!!`);
+// }
+// greetuser();
+
+   // Question no 03 //
+
+//    function addnum(){
+//     var Firstnumber = prompt('Enter your First Number!');
+//     var secondNumber = prompt('Enter your second Number!');
+//     var num1 = parseFloat(Firstnumber);
+//     var num2 = parseFloat(secondNumber);
+//     var sum = num1 + num2;
+//     alert(`Your Answer is ${sum}!`);
+// }
+
+// addnum();
